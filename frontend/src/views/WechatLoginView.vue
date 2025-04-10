4c4c30da30df879f6706df7c78412577d48fa3e7
heart-fires <2670681303@qq.com>
2025-04-10 16:44:24 +0800

    Add files via upload

--- frontend/src/views/WechatLoginView.vue
+++ frontend/src/views/WechatLoginView.vue
@@ -5,13 +5,20 @@
 </template>
 
 <script setup>
+import axios from 'axios';
 
-// 这里需要替换为你自己的微信开放平台的 APPID 和回调地址
-const APPID = 'your_appid';
-const REDIRECT_URI = encodeURIComponent('http://yourdomain.com/wechat/callback');
-const SCOPE = 'snsapi_userinfo';
+const getWechatConfig = async () => {
+  try {
+    const response = await axios.get('/wechat/config/');
+    return response.data;
+  } catch (error) {
+    console.error('获取微信配置失败:', error);
+    return {};
+  }
+};
 
-const redirectToWechatLogin = () => {
+const redirectToWechatLogin = async () => {
+  const { APPID, REDIRECT_URI, SCOPE } = await getWechatConfig();
   const wechatLoginUrl = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${APPID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=${SCOPE}&state=STATE#wechat_redirect`;
   window.location.href = wechatLoginUrl;
 };
